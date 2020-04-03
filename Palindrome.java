9752c5e16aa82e8305bd513c00d835de85d1cc5f
airi <59892547+airicognize@users.noreply.github.com>
2020-04-03 13:06:08 -0400

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Palindrome{
+  public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+
+     System.out.println("Please input a word for us to check if it is a Palindrome.");
+     String input = sc.nextLine().trim();
+     String reverse = "";
+
+     String inputL = input.toLowerCase();
+     String inputf= inputL.replaceAll("\\s","");
+     String inputt= inputL.replaceAll("\\W","");
+
+       for(int i = inputt.length() - 1; i >= 0; i--)
+       {
+        reverse = reverse + inputt.charAt(i);
+       }
+
+      String reversedL = reverse.toLowerCase();
+
+      if (reversedL.equals(inputt)) {
+        System.out.println("Your input is a Palindrome.");
+      } else {
+        System.out.println("Your input is not a Palindrome");
+
+      }
+  }
+}
