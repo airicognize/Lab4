a9ddce9a19931eeda68038164e0c61471cc35337
airi <59892547+airicognize@users.noreply.github.com>
2020-04-03 13:18:25 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,9 @@
+/**
+ * @author Jeremie Guerchon
+ * a program that receives a word as an input from the user and checks if it is a palindrome: if it reads the same backward as forward.
+ *
+ */
+
 import java.util.*;
 
 public class Palindrome{
